2f15ca57d1fed4b8fea0f27c299f923024227d9e
Poliwkaa <poliwkaa92@gmail.com>
2021-12-10 01:11:44 +0300

    fixed bug, added info, best-restaurant, feedback and apps sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function(){
+  $('.feedback__inner').slick({
+    dots: true,
+  });
 
+  
+  
   $(function(){
-    var mixer = mixitup('.categories__cards');
+    var mixer = mixitup('.top-categories__cards', {
+      load: {
+      filter: '.burger'
+      }
     });
-
+    });
 })
